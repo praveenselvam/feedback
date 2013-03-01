cd72ce3e662b12d66a40c96e81335fa67c7681fa
Praveen Selvam <praveen.selvam@gmail.com>
2013-03-01 18:33:10 +0530

    Praveen/Raj/Jeevan - First commit to setup base.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feedback::Application.config.secret_token = '10e3610e988c988048bf32acc8266a6fdf63959eb634c2251ba225796b988ab1a93e75d5a8deed0bf2ca2809dd322b00afd60793c60e5e4f4053a5f248325407'
